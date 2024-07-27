ef43670d5d41023199e6e1980ae24f788da17bab
Vinol Noronha <vinolnoronha7@gmail.com>
2024-07-27 11:09:10 +0530

    Add files via upload

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const page = () => {
+  return;
+  <>
+    <h1 className="text-red-700">Vinol Noronha</h1>
+    <p className="text-black-400 ">
+      I am an aspiring web developer who is passionate and curious to learn and
+      explore new things in this field of web development. Currently pursuing my
+      BE in AI and ML, beyond my technical interests I'm an aviation enthusiast
+      and automotive enthusiast who gets fascinated by seeing these beautiful
+      machines. I enjoy clicking the pictures of nature when it shows its best
+      form!!
+    </p>
+    <p className="text-gray-500 ">The week 1 has been great!.</p>
+  </>;
+};
+
+export default page;
